47cea16bc4b18db6772d3d3ad50fd6c33f314705
WendelRodriguesz <wendeldev2010@gmail.com>
2025-08-19 13:05:03 -0300

    ✨ feat: Adiciona configuração do Swagger para documentação da API e refatora o arquivo main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { configurarSwagger } from './shared/swagger/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-
+  configurarSwagger(app);
   const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
   await app.listen(port);
 
