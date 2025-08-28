634350df4d6c39a7285d0e30c0904f2e83fc0894
WendelRodriguesz <wendeldev2010@gmail.com>
2025-08-28 16:23:43 -0300

    refactor: organiza módulos no AppModule e remove validação de sigla no SiglaUfDto

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,10 +5,14 @@ import { AuthModule } from './modules/auth/auth.module';
 import { AuthGuard } from '@nestjs/passport';
 import { ConfigModule } from '@nestjs/config';
 import { ReferenciasModule } from './modules/referencias/referencias.module';
+import { ProfissionalModule } from './modules/profissional/profissional.module';
+import { ConsultaModule } from './modules/consulta/consulta.module';
+import { PacienteModule } from './modules/paciente/paciente.module';
+import { PlanoAlimentarModule } from './modules/plano-alimentar/plano-alimentar.module';
 
 @Global()
 @Module({
-  imports: [UsuarioModule, DatabaseModule, AuthModule, ConfigModule.forRoot({ isGlobal: true }), ReferenciasModule,],
+  imports: [UsuarioModule, DatabaseModule, AuthModule, ConfigModule.forRoot({ isGlobal: true }), ReferenciasModule, ProfissionalModule, ConsultaModule, PacienteModule, PlanoAlimentarModule ],
   controllers: [],
   providers: [
     // {
